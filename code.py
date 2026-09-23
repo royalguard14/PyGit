61422fb0d4cdd185b5835f88796b309dc337d8ad
Clerk-Zar <ghaizar.bautista013@gmail.com>
2026-09-23 09:16:32 +0800

    try zar

--- code.py
+++ code.py
@@ -1,10 +1,10 @@
+import os
+
 class HelloWorld:
     def run(self):
+        os.system("cls")
         print("Hello World!")
-        print("🔥 PyGit VERSION 2.0.0")
-        print("CLASS EXECUTION SUCCESSFUL!")
-        print("🚀 PRODUCTION LIVE UPDATE ENGINE")
-        print("Running from GitHub: code.py")
+        print("🔥 PyGit VERSION 2.1.0")
 
 
 if __name__ == "__main__":
